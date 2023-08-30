bff3c1144044d1742711d8d4a665a70301c046fa
sanjaydambal <77163257+sanjaydambal@users.noreply.github.com>
2023-08-30 09:44:49 +0530

    improved by adding download button

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
   const textInput = document.getElementById("textInput");
   const generateButton = document.getElementById("generateButton");
+  const downloadButton = document.getElementById("downloadButton");
   const qrcodeContainer = document.getElementById("qrcode");
+  let qrImageBlob = null;
 
   generateButton.addEventListener("click", async function () {
     const text = textInput.value;
     if (text.trim() !== "") {
       // Clear existing QR code
       qrcodeContainer.innerHTML = "";
+      downloadButton.disabled = true;
 
       // Generate QR code using API
       try {
         const response = await fetch(`https://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(text)}`);
-        const qrImageURL = await response.url;
+        qrImageBlob = await response.blob();
 
         const img = document.createElement("img");
-        img.src = qrImageURL;
+        img.src = URL.createObjectURL(qrImageBlob);
         qrcodeContainer.appendChild(img);
+        downloadButton.disabled = false;
       } catch (error) {
         qrcodeContainer.innerHTML = "<p class='error-message'>Error generating QR code</p>";
       }
+    }
+  });
+
+  downloadButton.addEventListener("click", function () {
+    if (qrImageBlob) {
+      // Create a temporary anchor element to trigger the download
+      const link = document.createElement("a");
+      link.href = URL.createObjectURL(qrImageBlob);
+      link.download = "qrcode.png";
+      link.click();
     }
   });
 });
